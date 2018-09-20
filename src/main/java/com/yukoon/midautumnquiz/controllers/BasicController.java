98d54f9edcbcf6d14fa1d02c032bf91d601b1abd
a5928497 <61722615@qq.com>
2018-09-21 02:14:23 +0800

    developing

---
+++ src/main/java/com/yukoon/midautumnquiz/controllers/BasicController.java
@@ -0,0 +1,12 @@
+package com.yukoon.midautumnquiz.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BasicController {
+	@GetMapping("/test")
+	public String test() {
+		return "public/index";
+	}
+}
