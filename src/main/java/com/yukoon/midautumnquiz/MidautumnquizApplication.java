ac3aa8ea700547dde17d49c51f3129ec2138d7f8
a5928497 <61722615@qq.com>
2018-09-21 01:18:43 +0800

    ready

---
+++ src/main/java/com/yukoon/midautumnquiz/MidautumnquizApplication.java
@@ -0,0 +1,12 @@
+package com.yukoon.midautumnquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidautumnquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidautumnquizApplication.class, args);
+	}
+}
